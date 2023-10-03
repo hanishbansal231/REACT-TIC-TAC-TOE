59010ed4f0378342c148f42d79c09a19ce00701f
hanishbansal231 <hanishbansal803@gmail.com>
2023-10-03 10:54:24 +0530

    Create a tic tac toe game

---
+++ src/Components/Grid/Grid.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Card from "../Card/Card";
+import './Grid.css'
+import isWinner from "../../helper/CheckWinner";
+function Grid({ numberOfCards }) {
+    const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+    const [turn, setTurn] = useState(true);
+    const [winner, setWinner] = useState(null);
+    function play(index) {
+        if (turn == true) {
+            board[index] = 'O';
+        } else {
+            board[index] = 'X';
+        }
+        const win = isWinner(board, turn ? 'O' : 'X');
+        if (win) {
+            setWinner(win);
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
+    function reset() {
+        setTurn(true);
+        setWinner(null);
+        setBoard(Array(numberOfCards).fill(""));
+    }
+    return (
+        <div className="grid-wrapper">
+            <h1 className="turn-highlight">
+                {
+                    winner ? (<div></div>) : (<p>Current Turn: {(turn) ? 'O' : 'X'}</p>)
+                }
+            </h1>
+            <div className="grid">
+                {
+                    board.map((el, idx) => (
+                        <Card gameEnd={winner ? true : false} key={idx} onPlay={play} player={el} index={idx} />
+                    ))
+                }
+            </div>
+            {
+                winner && (
+                    <div className="winner-box">
+                        <h1 className="turn-highlight">
+                            Winner is {winner}
+                        </h1>
+                        <button className="reset" onClick={reset}> Reset Game</button>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default Grid;
